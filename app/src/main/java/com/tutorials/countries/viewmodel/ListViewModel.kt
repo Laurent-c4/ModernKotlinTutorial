b5037112a90e76ea436bc29bdbf094853aa5d152
Laurent-c4 <laurentjuma.c4@gmail.com>
2021-05-02 05:08:19 +0300

    Implement Viewmodel

--- app/src/main/java/com/tutorials/countries/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/tutorials/countries/viewmodel/ListViewModel.kt
@@ -1,7 +1,35 @@
 package com.tutorials.countries.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.tutorials.countries.model.Country
 
 class ListViewModel : ViewModel() {
+
+    val countries = MutableLiveData<List<Country>>()
+    val countryLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchCountries()
+    }
+
+    private fun fetchCountries() {
+        val mockData: List<Country> = listOf(
+            Country("Country A"),
+            Country("Country B"),
+            Country("Country C"),
+            Country("Country D"),
+            Country("Country E"),
+            Country("Country F"),
+            Country("Country I"),
+            Country("Country J"),
+            Country("Country K")
+        )
+        
+        countryLoadError.value = false
+        loading.value = false
+        countries.value = mockData
+    }
 
 }
