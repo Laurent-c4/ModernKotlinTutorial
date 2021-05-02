adeb72209a3e412002a3a983f3fe0e1d891d0c63
Laurent-c4 <laurentjuma.c4@gmail.com>
2021-05-02 14:32:41 +0300

    Implement Refresh

--- app/src/main/java/com/tutorials/countries/util/util.kt
+++ app/src/main/java/com/tutorials/countries/util/util.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.ImageView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.tutorials.countries.R
 
@@ -17,6 +18,7 @@ fun getProgressDrawable(context: Context) = CircularProgressDrawable(context)
 fun ImageView.loadImage(url: String?, progressDrawable: CircularProgressDrawable){
     val options = RequestOptions()
         .placeholder(progressDrawable)
+        .diskCacheStrategy(DiskCacheStrategy.ALL)
         .error(R.mipmap.ic_launcher_round)
     Glide.with(this.context)
         .setDefaultRequestOptions(options)
