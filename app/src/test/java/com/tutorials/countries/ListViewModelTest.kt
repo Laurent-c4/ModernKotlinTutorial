ac8a861e6752e5f9b8714dcb792ee0d8a940dc2c
Laurent-c4 <laurentjuma.c4@gmail.com>
2021-05-02 21:17:59 +0300

    Test the success path

--- app/src/test/java/com/tutorials/countries/ListViewModelTest.kt
+++ app/src/test/java/com/tutorials/countries/ListViewModelTest.kt
@@ -1,13 +1,24 @@
 package com.tutorials.countries
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tutorials.countries.model.CountriesService
+import com.tutorials.countries.model.Country
+import com.tutorials.countries.viewmodel.ListViewModel
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.plugins.RxAndroidPlugins
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.schedulers.ExecutorScheduler
 import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.MockitoAnnotations
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeUnit
 
@@ -15,6 +26,35 @@ class ListViewModelTest {
 
     @get:Rule
     var rule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var countriesService: CountriesService
+
+    @InjectMocks
+    var listViewModel = ListViewModel()
+
+    private var testSingle: Single<ArrayList<Country>>? = null
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+    }
+
+    @Test
+    fun getCountriesSuccess() {
+        val country = Country("countryName", "capital", "url")
+        val countryList = arrayListOf<Country>(country)
+
+        testSingle = Single.just(countryList)
+
+        `when`(countriesService.getCountries()).thenReturn(testSingle)
+
+        listViewModel.refresh()
+
+        Assert.assertEquals(1, listViewModel.countries.value?.size)
+        Assert.assertEquals(false, listViewModel.countryLoadError.value)
+        Assert.assertEquals(false, listViewModel.loading.value)
+    }
 
     @Before
     fun setUpRxSchedulers() {
