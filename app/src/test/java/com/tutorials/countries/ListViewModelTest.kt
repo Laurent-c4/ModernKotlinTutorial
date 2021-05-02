1a800221d645acbe09e0fc71a7f46525070fb14d
Laurent-c4 <laurentjuma.c4@gmail.com>
2021-05-02 20:57:31 +0300

    Setup Unit Test

---
+++ app/src/test/java/com/tutorials/countries/ListViewModelTest.kt
@@ -0,0 +1,47 @@
+package com.tutorials.countries
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.Scheduler
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.internal.schedulers.ExecutorScheduler
+import io.reactivex.plugins.RxJavaPlugins
+import org.junit.Before
+import org.junit.Rule
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
+
+class ListViewModelTest {
+
+    @get:Rule
+    var rule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUpRxSchedulers() {
+        val immediate = object : Scheduler() {
+            override fun scheduleDirect(run: Runnable?, delay: Long, unit: TimeUnit?): Disposable {
+                return super.scheduleDirect(run, 0, unit)
+            }
+
+            override fun createWorker(): Worker {
+                return ExecutorScheduler.ExecutorWorker(Executor { it.run() })
+            }
+        }
+
+        RxJavaPlugins.setInitIoSchedulerHandler {
+            immediate
+        }
+        RxJavaPlugins.setInitComputationSchedulerHandler {
+            immediate
+        }
+        RxJavaPlugins.setInitNewThreadSchedulerHandler {
+            immediate
+        }
+        RxJavaPlugins.setInitSingleSchedulerHandler {
+            immediate
+        }
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler {
+            immediate
+        }
+    }
+}
